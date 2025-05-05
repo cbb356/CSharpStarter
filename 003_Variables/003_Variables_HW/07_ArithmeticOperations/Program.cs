faf5933c47657f02b69ac9a7583ec209a7cb5b87
cbb356 <cbb356@gmail.com>
2025-05-06 01:20:35 +0300

    07 exercise for 003 Lessons edited

--- 003_Variables/003_Variables_HW/07_ArithmeticOperations/Program.cs
+++ 003_Variables/003_Variables_HW/07_ArithmeticOperations/Program.cs
@@ -4,11 +4,11 @@
     {
         static void Main(string[] args)
         {
-            int x = 9, y = 0;
+            int x = 9, y = 6;
             Console.WriteLine($"{x} + {y} = {x + y}");
             Console.WriteLine($"{x} - {y} = {x - y}");
             Console.WriteLine($"{x} * {y} = {x * y}");
-            //Console.WriteLine($"{x} / {y} = {(double)x / y}");
+            Console.WriteLine($"{x} / {y} = {(double)x / y}");
             Console.WriteLine($"{x} % {y} = {x % y}");
         }
     }
