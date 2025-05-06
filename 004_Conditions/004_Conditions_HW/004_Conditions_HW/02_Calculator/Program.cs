0b523ed951ab791d9f8b6b3b632747c3b7f93c12
cbb356 <cbb356@gmail.com>
2025-05-06 08:57:55 +0300

    Else blocks in Calculator removed

--- 004_Conditions/004_Conditions_HW/004_Conditions_HW/02_Calculator/Program.cs
+++ 004_Conditions/004_Conditions_HW/004_Conditions_HW/02_Calculator/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int operand1 = 6, operand2 = 4;
+            int operand1 = 6, operand2 = 0;
             Console.WriteLine("Enter the arithmetic operation (+, -, *, /, %):");
             string sign = Console.ReadLine();
 
@@ -32,10 +32,7 @@
                         Console.WriteLine($"You cannot divide by zero!");
                         break;
                     } 
-                    else
-                    {
-                        Console.WriteLine($"{operand1} {sign} {operand2} = {((double)operand1 / operand2)}");
-                    }                           
+                    Console.WriteLine($"{operand1} {sign} {operand2} = {((double)operand1 / operand2)}");
                     break;
                 }
                 case "%":
@@ -45,10 +42,7 @@
                         Console.WriteLine($"You cannot calculate the remainder of a division by zero!");
                         break;
                     } 
-                    else
-                    {
-                        Console.WriteLine($"{operand1} {sign} {operand2} = {(operand1 % operand2)}");
-                    }                           
+                    Console.WriteLine($"{operand1} {sign} {operand2} = {(operand1 % operand2)}");
                     break;
                 }
                 default:
