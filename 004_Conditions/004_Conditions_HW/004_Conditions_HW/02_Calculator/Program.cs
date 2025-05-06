33a196a650f3e9f2e3c14f749cc962dbb1d740f5
cbb356 <cbb356@gmail.com>
2025-05-06 08:50:42 +0300

    Some changes in Calculator program

--- 004_Conditions/004_Conditions_HW/004_Conditions_HW/02_Calculator/Program.cs
+++ 004_Conditions/004_Conditions_HW/004_Conditions_HW/02_Calculator/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int operand1 = 6, operand2 = 4;
-            Console.WriteLine("Enter the operation:");
+            Console.WriteLine("Enter the arithmetic operation (+, -, *, /, %):");
             string sign = Console.ReadLine();
 
             switch (sign) 
@@ -29,12 +29,25 @@
                 {
                     if (operand2 == 0)
                     {
-                        Console.WriteLine($"You can't divide by zero!");
+                        Console.WriteLine($"You cannot divide by zero!");
                         break;
                     } 
                     else
                     {
                         Console.WriteLine($"{operand1} {sign} {operand2} = {((double)operand1 / operand2)}");
+                    }                           
+                    break;
+                }
+                case "%":
+                {
+                    if (operand2 == 0)
+                    {
+                        Console.WriteLine($"You cannot calculate the remainder of a division by zero!");
+                        break;
+                    } 
+                    else
+                    {
+                        Console.WriteLine($"{operand1} {sign} {operand2} = {(operand1 % operand2)}");
                     }                           
                     break;
                 }
