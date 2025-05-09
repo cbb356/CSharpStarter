065d9d3add4af17077825ddb7a3853c136d24830
cbb356 <cbb356@gmail.com>
2025-05-09 22:13:32 +0300

    03_Range minor change

--- 004_Conditions/004_Conditions_HW/03_Range/Program.cs
+++ 004_Conditions/004_Conditions_HW/03_Range/Program.cs
@@ -7,7 +7,7 @@ namespace Range
         static void Main(string[] args)
         {
             int number;
-            Console.WriteLine("Enter an integer number:");
+            Console.WriteLine("Enter an integer [0-100]:");
             string input = Console.ReadLine();
 
             bool success = int.TryParse(input, out number);
@@ -20,16 +20,16 @@ namespace Range
                     case > 100:
                         Console.WriteLine($"Entered value is {number}; out of an acceptable range.");
                         break;
-                    case < 15: 
+                    case < 15:
                         Console.WriteLine($"Entered value {number} is in range [0-14]");
                         break;
-                    case < 36: 
+                    case < 36:
                         Console.WriteLine($"Entered value {number} is in range [15-35]");
                         break;
-                    case < 51: 
+                    case < 51:
                         Console.WriteLine($"Entered value {number} is in range [36-50]");
                         break;
-                    default: 
+                    default:
                         Console.WriteLine($"Entered value {number} is in range [51-100]");
                         break;
                 }
