ccdab6d2b22dcb8021cff8f7103a45d9c1340a24
cbb356 <cbb356@gmail.com>
2025-05-22 10:18:33 +0300

    Starter 009 HW some tasks added

--- 009_Arrays/009_Arrays_HW/02_ArrayEvaluation/Program.cs
+++ 009_Arrays/009_Arrays_HW/02_ArrayEvaluation/Program.cs
@@ -2,6 +2,47 @@
 {
     internal class Program
     {
+        static void ShowArray(string name, int[] array)
+        {
+            Console.Write(name);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < (array.Length - 1))
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        static int[] OddNumbersArray(int[] array)
+        {
+            int oddCount = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    oddCount++;
+                }
+            }
+
+            int[] result = new int[oddCount];
+            int currentIndex = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    result[currentIndex] = array[i];
+                    currentIndex++;
+                }
+            }
+
+            return result;
+        }
         static void Main(string[] args)
         {
             Console.Write("Enter the size of the array (positive integer N): ");
@@ -17,36 +58,21 @@
             // Fill the array with random integers.
             for (int i = 0; i < array.Length; i++)
             {
-                array[i] = rand.Next(-1000, 1001);
+                array[i] = rand.Next(-100, 101);
             }
 
-            Console.Write("Elements of the array: ");
-            for (int i = 0; i < array.Length; i++)
-            {
-                Console.Write(array[i]);
-                if (i < (array.Length - 1))
-                {
-                    Console.Write(", ");
-                }
-            }
+            ShowArray("Elements of the array: ", array);
 
-            Console.WriteLine($"\nMaximum value: {array.Max()}");
+            Console.WriteLine($"Maximum value: {array.Max()}");
             Console.WriteLine($"Minimum value: {array.Min()}");
             Console.WriteLine($"Sum of all array elements: {array.Sum()}");
             Console.WriteLine($"Average value: {array.Average()}");
-            Console.Write($"Odd values: ");
 
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] % 2 != 0)
-                {
-                    Console.Write(array[i]);
-                    if (i < (array.Length - 1))
-                    {
-                        Console.Write(", ");
-                    }
-                }
-            }
+            //Create subarray with odd numbers only
+            int[] oddNumbersArray = OddNumbersArray(array);
+            ShowArray("Odd values: ", oddNumbersArray);
+
+            Console.WriteLine();
 
             // Delay.
             Console.ReadKey();
